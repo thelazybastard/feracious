b15b38510f5316dc8089905b13586f8fc508a58d
Monish Giani <256585194+thelazybastard@users.noreply.github.com>
2026-08-26 17:02:23 +0700

    added argparse for pomodoro

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
+import argparse
+
 # accept and parse args
 def main():
-    print("Hello")
+    parser = argparse.ArgumentParser(description="All-In-One Productivity Suite in your Terminal")
+
+    parser.add_argument('-p', action='store_true', help="Starts Pomodoro Timer")
+
+    args = parser.parse_args()
+
+    print(args.p)
 
 
 if __name__ == "__main__":
