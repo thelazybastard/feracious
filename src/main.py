b6fd5a57e8bd61261e6ddc70ab01b8544bea2bc3
Monish Giani <256585194+thelazybastard@users.noreply.github.com>
2026-08-27 00:11:02 +0700

    use tkinter instead

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("my app")
+        self.geometry("400x150")
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        self.button = tk.Button(self, text="my button", command=self.button_callback)
+        self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+    
+        
+    def button_callback(self):
+        print("button pressed")
+
+app = App()
+app.mainloop()
